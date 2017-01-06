033626174aeb42d92e43e1e56b3a2a6643878611
Johannes Stein <johannesstein@freeze-dev.com>
2017-01-06 11:05:07 +0100

    Some animation

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import MovieItem from './MovieItem';
 
@@ -9,12 +10,19 @@ import generateImageName from './generateImageName';
 import './MovieList.css';
 
 const MovieList: () => React.Element<*> = ({ items }) => (
-  <div className="movie-list">{items.map(item => (
-    <MovieItem
-      key={item.id}
-      name={item.title}
-      image={generateImageName(item.poster_path)} />
-    ))}</div>
+  <div className="movie-list">
+    <ReactCSSTransitionGroup
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={500}
+      transitionName="movie-item">
+      {items.map(item => (
+      <MovieItem
+        key={item.id}
+        name={item.title}
+        image={generateImageName(item.poster_path)} />
+      ))}
+    </ReactCSSTransitionGroup>
+  </div>
 )
 
 export default MovieList;
